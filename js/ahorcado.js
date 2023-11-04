b5492222f917ce07da884e4a207dc0b20a7b25fe
AdrianC89 <107586138+AdrianC89@users.noreply.github.com>
2023-11-04 20:33:05 -0300

    Add files via upload
    Repositorio actual del proyecto, subido para deploy

---
+++ js/ahorcado.js
@@ -0,0 +1,205 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const pantallaInicial = document.getElementById("pantalla-inicial");
+    const botonComenzar = document.getElementById("comenzar");
+    const principaljuego = document.getElementById("game-container");
+    const musicaFondo = document.getElementById("musicaFondo");
+  
+    // Agrega un evento de clic al documento para iniciar la reproducción de la música
+    document.addEventListener("click", function() {
+        musicaFondo.play(); // Inicia la reproducción de la música al hacer clic en cualquier lugar de la página
+    });
+  
+    botonComenzar.addEventListener("click", function() {
+        pantallaInicial.style.display = "none"; // Oculta la pantalla inicial al presionar el botón "Comenzar Juego"
+        principaljuego.style.display = "block"; // Muestra el contenedor del juego al presionar el botón "Comenzar Juego"
+        startGame(); // Inicia el juego al presionar el botón "Comenzar Juego"
+    });
+  });
+
+
+
+// Variables
+const words = ["html", "css", "web", "programacion", "javascript"];
+let word = "";
+let guessedLetters = [];
+let attempts = 6;
+let hangmanStep = 0;
+let puntajeAcumulado = 0;
+
+// Iniciar juego al cargar la ventana
+window.onload = startGame;
+
+// Seleccionar una palabra aleatoria
+function selectRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+}
+
+// Iniciar el juego
+function startGame() {
+    word = selectRandomWord();
+    guessedLetters = [];
+    attempts = 6;
+    hangmanStep = 0;
+    puntajeAcumulado = 0;
+    updateUI();
+    enableKeyboard();
+}
+
+// Actualizar la interfaz de usuario
+function updateUI() {
+    updateWordContainer();
+    updateGuessesContainer();
+    updateHangmanImage();
+    updateKeyboard();
+    updatePuntajeAcumulado();
+}
+
+// Actualizar el contenedor de palabras
+function updateWordContainer() {
+    const wordContainer = document.getElementById("word-container");
+    let displayWord = "Palabra a adivinar: ";
+    for (const letter of word) {
+        if (guessedLetters.includes(letter.toLowerCase()) || !letter.match(/[a-zA-Z]/)) {
+            displayWord += letter;
+        } else {
+            displayWord += "_";
+        }
+    }
+    wordContainer.textContent = displayWord;
+}
+
+// Actualizar el contenedor de letras adivinadas
+function updateGuessesContainer() {
+    const guessesContainer = document.getElementById("guesses");
+    guessesContainer.textContent = "Letras adivinadas: " + guessedLetters.join(", ");
+}
+
+// Actualizar la imagen del ahorcado
+function updateHangmanImage() {
+    const hangmanImg = document.getElementById("hangman");
+    hangmanImg.src = "./Assets/ahorcado/hangman" + hangmanStep + ".png";
+}
+
+// Actualizar el teclado
+function updateKeyboard() {
+    const keyboardContainer = document.getElementById("keyboard");
+    keyboardContainer.innerHTML = "";
+    for (let i = 65; i <= 90; i++) {
+        const letter = String.fromCharCode(i);
+        const button = document.createElement("button");
+        button.textContent = letter;
+        button.addEventListener("click", () => makeGuess(letter));
+        if (guessedLetters.includes(letter.toLowerCase())) {
+            button.disabled = true;
+        }
+        keyboardContainer.appendChild(button);
+    }
+}
+
+// Actualizar el puntaje acumulado
+function updatePuntajeAcumulado() {
+    const puntajeAcumuladoElement = document.getElementById("puntaje-container");
+    puntajeAcumuladoElement.textContent = "Puntaje acumulado: " + puntajeAcumulado;
+}
+
+// Realizar una suposición
+function makeGuess(letter) {
+    if (!guessedLetters.includes(letter.toLowerCase())) {
+        guessedLetters.push(letter.toLowerCase());
+
+        if (!word.toLowerCase().includes(letter.toLowerCase())) {
+            attempts--;
+            hangmanStep++;
+            playSound("soundIncorrect");
+        } else {
+            puntajeAcumulado += 10;
+            playSound("soundCorrect");
+        }
+
+        updateUI();
+        checkGameOver();
+    }
+
+    if (getDisplayedWord().toLowerCase() === word.toLowerCase()) {
+        disableKeyboard();
+    }
+}
+
+function playSound(soundId) {
+    const sound = document.getElementById(soundId);
+    sound.play();
+}
+
+// Verificar si el juego ha terminado
+function checkGameOver() {
+    if (attempts === 0) {
+        const lossMessageText = `¡Perdiste! La palabra era '${word}'.`.toUpperCase();
+        mostrarModal(lossMessageText, true, false);
+        playSound("soundLose");
+        disableKeyboard();
+    } else if (getDisplayedWord().toLowerCase() === word.toLowerCase()) {
+        const victoryMessageText = `¡Ganaste! Adivinaste la palabra: ${word}.`.toUpperCase();
+        mostrarModal(victoryMessageText, true, true);
+        playSound("soundWin");
+        disableKeyboard();
+    }
+}
+
+// Obtener la palabra mostrada
+function getDisplayedWord() {
+    let displayedWord = "";
+    for (const letter of word) {
+        if (guessedLetters.includes(letter.toLowerCase()) || !letter.match(/[a-zA-Z]/)) {
+            displayedWord += letter;
+        } else {
+            displayedWord += "_";
+        }
+    }
+    return displayedWord;
+}
+
+// Mostrar mensaje de victoria o pérdida
+function mostrarModal(texto, mostrarBotonReinicio, esVictoria) {
+    const messageElement = esVictoria ? document.getElementById("victory-message") : document.getElementById("loss-message");
+    const messageText = esVictoria ? document.getElementById("victory-text") : document.getElementById("loss-text");
+    const restartButton = document.getElementById("restart-button");
+
+    messageText.textContent = texto;
+    messageElement.style.display = "block";
+
+    if (mostrarBotonReinicio) {
+        restartButton.style.display = "block";
+        restartButton.addEventListener("click", restartGame);
+    } else {
+        restartButton.style.display = "none";
+    }
+}
+
+// Deshabilitar el teclado
+function disableKeyboard() {
+    const keyboardButtons = document.querySelectorAll("#keyboard button");
+    keyboardButtons.forEach((button) => (button.disabled = true));
+}
+
+// Habilitar el teclado
+function enableKeyboard() {
+    const keyboardButtons = document.querySelectorAll("#keyboard button");
+    keyboardButtons.forEach((button) => (button.disabled = false));
+}
+
+// Reiniciar el juego
+function restartGame() {
+    startGame();
+    hideMessages();
+    enableKeyboard();
+}
+
+// Ocultar mensajes
+function hideMessages() {
+    const victoryMessage = document.getElementById("victory-message");
+    const lossMessage = document.getElementById("loss-message");
+
+    victoryMessage.style.display = "none";
+    lossMessage.style.display = "none";
+}
---
+++ js/tateti.js
@@ -0,0 +1,184 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const pantallaInicial = document.getElementById("pantalla-inicial");
+  const botonComenzar = document.getElementById("comenzar");
+  const principaljuego = document.getElementById("principaljuego");
+  const musicaFondo = document.getElementById("musicaFondo");
+
+  // Agrega un evento de clic al documento para iniciar la reproducción de la música
+  document.addEventListener("click", function() {
+      musicaFondo.play(); // Inicia la reproducción de la música al hacer clic en cualquier lugar de la página
+  });
+
+  botonComenzar.addEventListener("click", function() {
+      pantallaInicial.style.display = "none"; // Oculta la pantalla inicial al presionar el botón "Comenzar Juego"
+      principaljuego.style.display = "block"; // Muestra el contenedor del juego al presionar el botón "Comenzar Juego"
+      startGame(); // Inicia el juego al presionar el botón "Comenzar Juego"
+  });
+});
+
+
+
+
+const juegoTresEnLinea = (function () {
+    // Caracteres para el tablero
+    const x = "✖";
+    const o = "〇";
+  
+    // Elementos de la página
+    const cuadrados = document.querySelectorAll(".cuadrado");
+    const modal = document.querySelector("dialog");
+    const textoModal = modal.querySelector("h2");
+    let estadoJuego = "P1"; // P1 (Usuario) | P2 (Computadora) | PAUSA
+    let puntajeJugador1 = 0;
+    let puntajeJugador2 = 0;
+  
+    // Elementos de puntaje
+    const puntajeElement1 = document.getElementById("puntaje-jugador1");
+    const puntajeElement2 = document.getElementById("puntaje-jugador2");
+  
+    // Función para actualizar el indicador de turno y ficha
+    function actualizarIndicador() {
+      const jugadorActualElement = document.getElementById("jugador-actual");
+      const fichaActualElement = document.getElementById("ficha-actual");
+  
+      if (estadoJuego === "P1") {
+        jugadorActualElement.textContent = "Jugador 1";
+        fichaActualElement.textContent = x;
+      } else {
+        jugadorActualElement.textContent = "Computadora";
+        fichaActualElement.textContent = o;
+      }
+    }
+  
+    cuadrados.forEach((cuadrado, posicion) => {
+      cuadrado.addEventListener("click", () => {
+        if (estadoJuego === "PAUSA" || estadoJuego === "P2") return;
+        if (cuadrado.textContent !== "") return;
+    
+        // Reproducir sonido de X
+        const sonidoX = document.getElementById("sonidoX");
+        sonidoX.play();
+    
+        cuadrado.textContent = x;
+        const posicionGanadora = revisarSiHayGanador();
+  
+        if (typeof posicionGanadora === "object") {
+          ganar(posicionGanadora, "P1");
+          return;
+        }
+  
+        if (posicionGanadora === "empate") {
+          mostrarModal("Empate");
+        } else {
+          // Cambiar al siguiente jugador solo si no hay empate ni ganador
+          estadoJuego = "P2";
+          actualizarIndicador(); // Actualizar el indicador de turno y ficha
+          // Llamar a la función que permite a la computadora jugar
+          jugarComputadora();
+        }
+      });
+    });
+  
+    modal.querySelector("button").addEventListener("click", reiniciarJuego);
+  
+    function revisarSiHayGanador() {
+      const tablero = Array.from(cuadrados).map((cuadrado) => cuadrado.textContent);
+  
+      // Reviso filas
+      for (let i = 0; i < 9; i += 3) {
+        if (tablero[i] && tablero[i] === tablero[i + 1] && tablero[i] === tablero[i + 2]) {
+          return [i, i + 1, i + 2];
+        }
+      }
+  
+      // Reviso columnas
+      for (let i = 0; i < 3; i++) {
+        if (tablero[i] && tablero[i] === tablero[i + 3] && tablero[i] === tablero[i + 6]) {
+          return [i, i + 3, i + 6];
+        }
+      }
+  
+      // Reviso oblicuas
+      if (tablero[0] && tablero[0] === tablero[4] && tablero[0] === tablero[8]) return [0, 4, 8];
+      if (tablero[2] && tablero[2] === tablero[4] && tablero[2] === tablero[6]) return [2, 4, 6];
+  
+      // Reviso empate
+      if (tablero.includes("")) return false;
+      return "empate";
+    }
+  
+    function ganar(posicionesGanadoras, jugador) {
+      posicionesGanadoras.forEach((posicion) => cuadrados[posicion].classList.toggle("ganador", true));
+  
+      if (jugador === "P1") {
+          puntajeJugador1 += 3;
+          puntajeElement1.textContent = `Puntaje Jugador 1: ${puntajeJugador1}`;
+  
+          // Reproducir sonido de victoria
+          const sonidoGanar = document.getElementById("sonidoGanar");
+          sonidoGanar.play();
+  
+          mostrarModal("Gano el jugador 1");
+      } else {
+          puntajeJugador2 += 3;
+          puntajeElement2.textContent = `Puntaje Computadora: ${puntajeJugador2}`;
+  
+          // Reproducir sonido de perder
+          const sonidoPerder = document.getElementById("sonidoPerder");
+          sonidoPerder.play();
+  
+          mostrarModal("Gano la computadora");
+      }
+  }
+  
+    function mostrarModal(texto) {
+      textoModal.innerText = texto;
+      modal.showModal();
+      estadoJuego = "PAUSA";
+    }
+  
+    function reiniciarJuego() {
+      cuadrados.forEach((cuadrado) => {
+        cuadrado.textContent = "";
+        cuadrado.classList.toggle("ganador", false);
+      });
+      modal.close();
+      estadoJuego = "P1";
+      actualizarIndicador(); // Restablecer el indicador de turno y ficha
+    }
+  
+    function jugarComputadora() {
+      if (estadoJuego !== "P2") return;
+  
+      // Simulamos un retraso para que parezca que la computadora está pensando
+      setTimeout(() => {
+        const casillasVacias = Array.from(cuadrados).filter((cuadrado) => cuadrado.textContent === "");
+        if (casillasVacias.length > 0) {
+          const indiceAleatorio = Math.floor(Math.random() * casillasVacias.length);
+          const casillaAleatoria = casillasVacias[indiceAleatorio];
+          casillaAleatoria.textContent = o;
+  
+          const posicionGanadora = revisarSiHayGanador();
+          if (typeof posicionGanadora === "object") {
+            ganar(posicionGanadora, "P2");
+            return;
+          }
+  
+          if (posicionGanadora === "empate") {
+            mostrarModal("Empate");
+          } else {
+            estadoJuego = "P1";
+            actualizarIndicador(); // Actualizar el indicador de turno y ficha
+          }
+        }
+      }, 1500); // Retraso de 1.5 segundos (1500 milisegundos)
+    }
+  
+    // Llamamos a la función para iniciar el juego
+    actualizarIndicador();
+  
+    // Devuelve un objeto que expone solo las funciones y variables necesarias
+    return {
+      reiniciarJuego: reiniciarJuego,
+    };
+  })();
